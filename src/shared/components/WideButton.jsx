ae3f1dc8a8ce2e3f18d1bb1fbd20f1ad7c208f75
DeyKast <deykast@gmail.com>
2024-01-21 22:07:52 +0200

    create-first-onboarding-screen-and-add-new-packages

---
+++ src/shared/components/WideButton.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const WideButton = ({ label, width, onPressFunc }) => {
+  return (
+    <TouchableOpacity
+      style={[styles.WideButton, { width }]}
+      onPress={() => {
+        onPressFunc();
+      }}
+    >
+      <Text style={styles.ButtonText}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  WideButton: {
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "10px 24px",
+    backgroundColor: "#6750A4",
+    borderRadius: 100,
+  },
+  ButtonText: {
+    color: "#FFF",
+    fontWeight: "500",
+    fontSize: 14,
+    lineHeight: 20 /* 142.857% */,
+    letterSpacing: 0.1,
+  },
+});
+
+export default WideButton;
