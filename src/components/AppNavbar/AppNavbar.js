9a89265c7a516fdaf837d4063020b6662579639e
IraHrechyn <142835111+IraHrechyn@users.noreply.github.com>
2024-02-12 10:49:03 +0200

    Update AppNavbar.js
    code update

--- src/components/AppNavbar/AppNavbar.js
+++ src/components/AppNavbar/AppNavbar.js
@@ -1,47 +1,38 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
-
-import ScheduleIcon from '../../../assets/schedule.svg';
-import NotesIcon from '../../../assets/notes.svg';
-import SettingsIcon from '../../../assets/settings.svg';
-import InformationIcon from '../../../assets/info.svg';
-
+import { Image, StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import ScheduleIcon from '../../../assets/schedule.png';
+import NotesIcon from '../../../assets/notes.png';
+import SettingsIcon from '../../../assets/setting.png';
+import InformationIcon from '../../../assets/info.png';
 import Schedule from '../Schedule/Schedule';
 import NotesList from '../Notes/NotesList';
 import Settings from '../Settings/Settings';
 import Information from '../Information/Information';
 
 export default function AppNavbar() {
-    const [screenWidth, setScreenWidth] = useState(Dimensions.get('window').width);
-
-    useEffect(() => {
-        const updateScreenWidth = () => {setScreenWidth(Dimensions.get('window').width);};
-        Dimensions.addEventListener('change', updateScreenWidth);
-        return () => { Dimensions.removeEventListener('change', updateScreenWidth);};
-    }, []);
-
+    const window = useWindowDimensions();
+    
     const Tab = createBottomTabNavigator();
 
     const tabBarIconStyle = (focused) => ({
         ...styles.icon,
-        backgroundColor: focused ? '#4A4458' : '#332D41',
-        marginTop: screenWidth <= 768 ? 7 : 0,
+        backgroundColor: focused ? '#4A4458' : '#201f25',
+        marginTop: window.width <= 768 ? 7 : 0,
     });
 
     const tabBarLabelStyle = {
         ...styles.label,
-        marginBottom: screenWidth <= 768 ? 7 : 0,
-        marginLeft: screenWidth <= 768 ? 0 : 30,
+        marginBottom: window.width <= 768 ? 7 : 0,
+        marginLeft: window.width <= 768 ? 0 : 30,
     };
-      
+
     const getTabScreenOptions = (label, icon) => ({
         tabBarLabel: () => <Text style={tabBarLabelStyle}>{label}</Text>,
         tabBarIcon: ({ focused }) => (
-          <View style={tabBarIconStyle(focused)}>
-            <Image source={icon} style={{ width: 20, height: 20 }} />
-          </View>
+            <View style={tabBarIconStyle(focused)}>
+                <Image source={icon} style={{ width: 20, height: 20 }} />
+            </View>
         ),
     });
 
@@ -49,32 +40,15 @@ export default function AppNavbar() {
         <Tab.Navigator
             screenOptions={{
                 tabBarStyle: {
-                    backgroundColor: '#332D41',
+                    backgroundColor: '#201f25',
                     height: 60,
                     borderTopWidth: 0,
                 },
-            }}
-        >
-            <Tab.Screen
-                name="Розклад"
-                component={Schedule}
-                options={getTabScreenOptions('Розклад', ScheduleIcon)}
-            />
-            <Tab.Screen
-                name="Нотатки"
-                component={NotesList}
-                options={getTabScreenOptions('Нотатки', NotesIcon)}
-            />
-            <Tab.Screen
-                name="Налаштування"
-                component={Settings}
-                options={getTabScreenOptions('Налаштування', SettingsIcon)}
-            />
-            <Tab.Screen
-                name="Інформація"
-                component={Information}
-                options={getTabScreenOptions('Інформація', InformationIcon)}
-            />
+            }}>
+            <Tab.Screen name="Розклад" component={Schedule} options={getTabScreenOptions('Розклад', ScheduleIcon)} />
+            <Tab.Screen name="Нотатки" component={NotesList} options={getTabScreenOptions('Нотатки', NotesIcon)} />
+            <Tab.Screen name="Налаштування" component={Settings} options={getTabScreenOptions('Налаштування', SettingsIcon)}/>
+            <Tab.Screen name="Інформація" component={Information} options={getTabScreenOptions('Інформація', InformationIcon)} />
         </Tab.Navigator>
     );
 }
@@ -88,9 +62,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-
     label: {
         color: 'white',
         fontSize: 11,
-    }
+    },
 });
