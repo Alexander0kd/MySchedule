dc0c718c10382ed40968ddebf4a641c8f8bc092a
IraHrechyn <142835111+IraHrechyn@users.noreply.github.com>
2024-02-12 11:02:15 +0200

    Update Reminder.jsx

--- src/components/Schedule/Reminder.jsx
+++ src/components/Schedule/Reminder.jsx
@@ -1,4 +1,4 @@
-import React, { useState, createElement } from 'react';
+import React, { useState } from 'react';
 import { StatusBar, View } from 'react-native';
 import RNDateTimePicker from '@react-native-community/datetimepicker';
 
@@ -7,29 +7,29 @@ export default function Reminder({ onHide, onDismissed }) {
     const [mode] = useState('time');
 
     const onChange = (event, selectedDate) => {
+
         if (event.type === 'set' || event.type === 'confirm') {
             const currentDate = selectedDate || date;
             onHide();
-            onDismissed(false);
+            onDismissed(true);
             setDate(currentDate);
         }
         if (event.type === 'dismissed' || event.type === 'neutralButtonPressed') {
-            onDismissed(true);
             onHide();
         }
     };
     return (
-        <View>
-            <RNDateTimePicker
-                testID="dateTimePicker"
-                value={date}
-                mode={mode}
-                is24Hour={true}
-                display="default"
-                onChange={onChange}
-                themeVariant="dark"
-            />
-            <StatusBar style="auto" />
-        </View>
+      <View>
+          <RNDateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={true}
+            display="default"
+            onChange={onChange}
+            themeVariant="dark"
+          />
+          <StatusBar style="auto" />
+      </View>
     );
 }
