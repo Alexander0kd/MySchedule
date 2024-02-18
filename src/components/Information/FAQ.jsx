4f9f03da17fd40a902f8a6ee192f5f0f3d06d956
victoriababala <victoriababala1106@gmail.com>
2024-02-18 23:06:32 +0200

    created FAQ page

--- src/components/Information/FAQ.jsx
+++ src/components/Information/FAQ.jsx
@@ -1,11 +1,45 @@
 import * as React from 'react';
-import { Text, View, StatusBar } from 'react-native';
-
+import { Text, View, StatusBar, StyleSheet, ScrollView } from 'react-native';
+import FAQDropdown from './FAQDropdown';
+const DATA = [
+    {
+        id: 'Q1',
+        title: 'Як редагувати профіль',
+        description:
+            '1.Перейдіть на сторінку "Налаштування". <br/>2.Натисніть на кнопку "Налаштування профілів". <br/>3.Натисніть на профіль, який хочете редагувати.<br/>4.Натисніть на кнопку "Редагувати".',
+    },
+    {
+        id: 'Q2',
+        title: 'Як змінити профіль',
+        description: 'Натисніть на значок профіля у правому верхньому кутку екрану та оберіть бажаний профіль з переліку.',
+    },
+    {
+        id: 'Q3',
+        title: 'Як приховати предмети',
+        description:
+            '1.Перейдіть на сторінку "Налаштування".<br/>2.Натисніть на кнопку "Налаштування занять".<br/>3.Натисніть на значок "ока" біля назви предмету, який хочете приховати.',
+    },
+];
 export default function FAQ() {
     return (
         <View>
-            <Text>FAQ</Text>
             <StatusBar style="auto" />
+            <ScrollView>
+                <Text style={styles.text}>Профілі</Text>
+                <FAQDropdown key={DATA[0].id} question={DATA[0]} />
+                <FAQDropdown key={DATA[1].id} question={DATA[1]} />
+                <Text style={styles.text}>Приховування предметів</Text>
+                <FAQDropdown key={DATA[2].id} question={DATA[2]} />
+            </ScrollView>
         </View>
     );
 }
+const styles = StyleSheet.create({
+    text: {
+        marginTop: 16,
+        marginBottom: 16,
+        marginLeft: 24,
+        color: 'white',
+        fontSize: 22,
+    },
+});
---
+++ src/components/Information/FAQDropdown.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import { View, Text, Pressable, StyleSheet, TouchableOpacity, Image, useWindowDimensions } from 'react-native';
+import Arrow from '../../../assets/arrow_drop_down.png';
+
+const FAQDropdown = ({ question }) => {
+    const [arrowRotation, setArrowRotation] = useState(0);
+    const [isDropdownVisible, setDropdownVisible] = useState(false);
+    const toggleDropdown = () => {
+        setDropdownVisible(!isDropdownVisible);
+        setArrowRotation(arrowRotation === 0 ? 180 : 0);
+    };
+
+    return (
+        <View style={styles.container}>
+            <Pressable onPress={toggleDropdown}>
+                <View style={styles.title}>
+                    <View>
+                        <Text style={{ ...styles.buttonText, fontWeight: '500' }}>{question.title}</Text>
+                    </View>
+                    <Image source={Arrow} style={{ ...styles.arrow, transform: [{ rotate: `${arrowRotation}deg` }] }} />
+                </View>
+            </Pressable>
+            {isDropdownVisible && (
+                <View style={styles.dropdownContent}>
+                    <View style={styles.rowContainer}>
+                        <Text style={styles.infoText}>{question.description.split('<br/>').join('\n')}</Text>
+                    </View>
+                </View>
+            )}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 16,
+        marginRight: 24,
+        marginLeft: 24,
+        borderRadius: 16,
+
+        backgroundColor: '#332D41',
+        border: 0,
+    },
+
+    title: {
+        padding: 16,
+        paddingLeft: 24,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    arrow: {
+        width: 10,
+        height: 5,
+        justifyContent: 'flex-end',
+        marginTop: 12,
+        marginRight: 12,
+    },
+
+    dropdownContent: {
+        padding: 24,
+        paddingTop: 8,
+        backgroundColor: '#1D192B',
+        borderTopStartRadius: 0,
+        borderTopEndRadius: 0,
+        borderRadius: 10,
+        flex: 0,
+        paddingBottom: 32,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+        color: '#FFFFFF',
+        fontSize: 16,
+        lineHeight: 20,
+        letterSpacing: 0.1,
+    },
+    infoText: {
+        // fontSize: 14,
+        // color: 'white',
+        paddingBottom: 8,
+        paddingTop: 8,
+        color: 'white',
+        color: '#FFFFFF',
+        fontSize: 16,
+        lineHeight: 20,
+        letterSpacing: 0.1,
+    },
+
+    rowContainer: {
+        flexDirection: 'row',
+        gap: 14,
+    },
+});
+
+export default FAQDropdown;
