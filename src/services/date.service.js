b69bd5a961165e23f0a2d46a647a363a2e41256f
IraHrechyn <142835111+IraHrechyn@users.noreply.github.com>
2024-02-18 18:35:33 +0200

    Update date.service.js

--- src/services/date.service.js
+++ src/services/date.service.js
@@ -26,9 +26,6 @@ export async function getGroupList(url, facultyId, year) {
     }
 }
 
-
-
-
 export function getFacultyList() {
     const faculties = Object.keys(FacultyData).map(key => ({
         key: key,
@@ -48,8 +45,7 @@ export async function getGroupSchedule(url, groupId, year, facultyId) {
     //     // Заміна одинарних лапок на подвійні лапки для ключів та значень
     //     data = data.replace(/(\w+)\s*:/g, '"$1":');
     //     data = data.replace(/'([^']+)'/g, '"$1"');
-    //
-    //     // Розбиваємо рядок JSON на об'єкт JavaScript
+    
     //     const scheduleItems = JSON.parse(data);
     //
     //    // console.log('Group Schedule:', scheduleItems);
@@ -61,38 +57,4 @@ export async function getGroupSchedule(url, groupId, year, facultyId) {
 }
 
 
-
-
-// export async function getGroupSchedule(url, groupId, year, facultyId) {
-//     const fullURL = `${url}${year}/static/groups/${facultyId}/${groupId}/index.js`;
-//     console.log(fullURL);
-//    // try {
-//         const response = await axios.get(fullURL);
-//         let data = response.data;
-//         data = data.replace("const PNUschedule=", '');
-//         //data = data.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2":');
-//         //console.log("groupsData ", data);
-//         const groupsData = JSON.stringify(data);
-//        // console.log("groupsData ", groupsData);
-//         const scheduleItems = data.map(item => new ScheduleItem(item));
-//         console.log('Group Schedule:', scheduleItems);
-//         //return groupsData;
-//     // } catch (error) {
-//     //     console.error('Error fetching group schedule:', error);
-//     //     return {};
-//     // }
-// }
-
-
-    // return axios.get(fullURL)
-    //     .then(response => {
-    //         const scheduleItems = response.data.map(item => new ScheduleItem(item));
-    //         console.log('Group Schedule:', scheduleItems);
-    //         return scheduleItems;
-    //     })
-    //     .catch(error => {
-    //         console.error('Error fetching group schedule:', error);
-    //         return []; // Повертаємо порожній масив у разі помилки
-    //     });
-//}
 
