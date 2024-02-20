894b13e4ac095c04d182c0768695db39940e2255
IraHrechyn <142835111+IraHrechyn@users.noreply.github.com>
2024-02-20 04:06:37 +0200

    Add files via upload

---
+++ src/services/general-model.js
@@ -0,0 +1,38 @@
+import {useState} from "react";
+
+export default function GeneralModel(){
+    const [facultyId, updateFacultyId] = useState(null);
+    const [year, updateYear] = useState(null);
+    const [groupId, updateGroupId] = useState(null);
+
+    const data = {
+        facultyId,
+        year,
+        groupId
+    };
+
+    function setFacultyId(id) {
+        updateFacultyId(id);
+        data.facultyId = id;
+    }
+
+    function setYear(newYear) {
+        updateYear(newYear);
+        data.year = newYear;
+    }
+
+    function setGroupId(id) {
+        updateGroupId(id);
+        data.groupId = id;
+    }
+
+    return {
+        setFacultyId,
+        setYear,
+        setGroupId,
+        facultyId,
+        year,
+        groupId,
+        data
+    };
+};
