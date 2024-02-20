b8af2adf72eab6abfb998efac070a68ab599b27b
IraHrechyn <142835111+IraHrechyn@users.noreply.github.com>
2024-02-20 03:45:28 +0200

    Update DateBlock.jsx

--- src/components/Schedule/DateBlock.jsx
+++ src/components/Schedule/DateBlock.jsx
@@ -1,42 +1,50 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import Arrow from '../../../assets/arrow-left.png'
 
-const DateBlock = ({ date, onBackward, onForward }) => {
-    const formattedDate = new Intl.DateTimeFormat('uk-UA', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-    }).format(date);
+const DateBlock = ({ title, date, onBackward, onForward }) => {
+  const formattedDate = new Intl.DateTimeFormat('uk-UA', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }).format(date);
 
-    return (
-        <View style={styles.panelContainer}>
-            <TouchableOpacity onPress={onBackward}>
-                <Text style={styles.arrowText}>{'<'}</Text>
-            </TouchableOpacity>
-            <Text style={styles.dateText}>{formattedDate}</Text>
-            <TouchableOpacity onPress={onForward}>
-                <Text style={styles.arrowText}>{'>'}</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  return (
+    <View style={styles.panelContainer}>
+      <TouchableOpacity onPress={onBackward}>
+        <Image source={Arrow} style={styles.arrowImage} />
+      </TouchableOpacity>
+      <Text style={styles.dateText}>{formattedDate}</Text>
+      <TouchableOpacity onPress={onForward}>
+        <Image source={Arrow} style={[styles.arrowImage, { transform: [{ rotate: '180deg' }] }]} />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    panelContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#201f25',
-    },
-    arrowText: {
-        color: 'white',
-        fontSize: 20,
-        padding: 16,
-    },
-    dateText: {
-        color: 'white',
-        fontSize: 16,
-    },
+  panelContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#201f25',
+    top: 0,
+    left: 0,
+    right: 0,
+    position: 'absolute',
+    zIndex: 1,
+  },
+  arrowImage: {
+    width: 13,
+    height: 13,
+    resizeMode: 'contain',
+    tintColor: 'white',
+    margin: 16,
+  },
+  dateText: {
+    color: 'white',
+    fontSize: 16,
+  },
 });
 
 export default DateBlock;
