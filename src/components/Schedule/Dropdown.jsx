86b7b422b2f2750323c59389449b9d5f93bed573
IraHrechyn <142835111+IraHrechyn@users.noreply.github.com>
2024-02-25 18:34:50 +0200

    Update Dropdown.jsx

--- src/components/Schedule/Dropdown.jsx
+++ src/components/Schedule/Dropdown.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, Pressable, StyleSheet, TouchableOpacity, Image, useWindowDimensions } from 'react-native';
+import { View, Text, Pressable, StyleSheet, TouchableOpacity, Image, useWindowDimensions, Linking } from 'react-native';
 import Bell from '../../../assets/bell.png';
 import CrossedOutCall from '../../../assets/CrossedOutCall.png';
 import Arrow from '../../../assets/arrow_drop_down.png';
-import Reminder from './Reminder';
-import { useNavigation } from '@react-navigation/native';
+import Reminder from "./Reminder";
+import { useNavigation } from "@react-navigation/native";
 
 const DropDown = ({ lesson }) => {
     const [isDropdownVisible, setDropdownVisible] = useState(false);
@@ -46,10 +46,13 @@ const DropDown = ({ lesson }) => {
             <Pressable onPress={toggleDropdown}>
                 <View style={styles.title}>
                     <View>
-                        <Text style={{ ...styles.buttonText, fontWeight: '500' }}>
-                            {lesson.l.length > maxCharacters ? lesson.l.slice(0, 30) + '...' : lesson.l}
-                        </Text>
-                        <Text style={{ color: '#CAC4D0', fontSize: 12 }}>{lesson.lt === '(Л)' ? 'Лекція' : 'Лабораторна робота'}</Text>
+                        <View style={{ flexDirection: "row" }}>
+                            <Text style={{ color: 'white', fontWeight: '500' }}>{lesson.li}. </Text>
+                            <Text style={{ color: 'white', fontWeight: '500' }}>
+                                {lesson.l.length > maxCharacters ? lesson.l.slice(0, 30) + '...' : lesson.l}
+                            </Text>
+                        </View>
+                        <Text style={{ color: '#CAC4D0', fontSize: 12 }}>{lesson.lt==='(Л)'?"Лекція":"Лабораторна робота"}</Text>
                     </View>
                     <Image source={Arrow} style={{ ...styles.arrow, transform: [{ rotate: `${arrowRotation}deg` }] }} />
                 </View>
@@ -63,36 +66,46 @@ const DropDown = ({ lesson }) => {
                             <Text style={styles.infoText}>{lesson.g}</Text>
                         </View>
                         <View style={styles.rowContainer}>
-                            <Text style={styles.titleText}>Аудиторія:</Text>
-                            <Text style={styles.infoText}>{lesson.vr}</Text>
+                            {lesson.vr ? (
+                                <>
+                                    <Text style={styles.titleText}>Аудиторія:</Text>
+                                    <Text style={styles.infoText}>{lesson.vr}</Text>
+                                </>
+                            ) : (
+                                <>
+                                    <Text style={styles.titleText}>Посилання:</Text>
+                                    <TouchableOpacity style={ {  flex:0.7}} onPress={() => Linking.openURL(lesson.link)}>
+                                        <Text style={{ color: 'blue', textDecorationLine: 'underline' }}>  {lesson.link && lesson.link.length > 30 ? lesson.link.slice(0, 30) + '...' : lesson.link}</Text>
+                                    </TouchableOpacity>
+                                </>
+                            )}
                         </View>
+
                         <View style={styles.rowContainer}>
                             <Text style={styles.titleText}>Викладач:</Text>
                             <Text style={styles.infoText}>{lesson.t}</Text>
                         </View>
                         <View style={styles.rowContainer}>
                             <Text style={styles.titleText}>Час:</Text>
-                            <Text style={styles.infoText}>
-                                {lesson.ls} | {lesson.le}
-                            </Text>
+                            <Text style={styles.infoText}>{lesson.ls} | {lesson.le}</Text>
                         </View>
                     </View>
                     <View style={styles.actions}>
-                        <TouchableOpacity style={[styles.button, styles.buttonFirst]} onPress={() => navigation.navigate('Нотатки')}>
+                        <TouchableOpacity style={[styles.button, styles.buttonFirst]} onPress={() => navigation.navigate("Нотатки")}>
                             <Text style={styles.buttonText}>Нотатки</Text>
                         </TouchableOpacity>
                         <TouchableOpacity style={[styles.button, styles.buttonSecond]} onPress={showReminderModal}>
-                            {!isDismissedReminder ? (
+                            {!isDismissedReminder ?
                                 <View style={styles.buttonContent}>
                                     <Image source={Bell} style={styles.icon} />
                                     <Text style={styles.buttonText}>Нагадати</Text>
                                 </View>
-                            ) : (
+                                :
                                 <TouchableOpacity style={[styles.buttonContent]} onPress={buttonState}>
                                     <Image source={CrossedOutCall} style={styles.icon} />
                                     <Text style={styles.buttonText}>Не нагадувати</Text>
                                 </TouchableOpacity>
-                            )}
+                            }
                         </TouchableOpacity>
                     </View>
                 </View>
@@ -117,50 +130,50 @@ const styles = StyleSheet.create({
     title: {
         padding: 16,
         paddingLeft: 24,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flexDirection: "row",
+        justifyContent: "space-between",
     },
 
     titleText: {
         fontSize: 14,
-        color: 'white',
+        color: "white",
         paddingBottom: 8,
         paddingTop: 8,
         fontWeight: '500',
-        flex: 0.3,
+        flex:0.3,
     },
 
     arrow: {
         width: 9,
         height: 5,
-        justifyContent: 'flex-end',
+        justifyContent: "flex-end",
         marginTop: 13,
         marginRight: 13,
     },
 
     info: {
-        flexDirection: 'column',
+        flexDirection: "column",
         gap: 8,
-        flex: 1,
+        flex:1
     },
 
     dropdownContent: {
         padding: 24,
         paddingTop: 8,
-        backgroundColor: '#1D192B',
+        backgroundColor: "#1D192B",
         borderTopStartRadius: 0,
         borderTopEndRadius: 0,
         borderRadius: 10,
         flex: 1,
-        paddingBottom: 32,
+        paddingBottom: 32
     },
 
     infoText: {
         fontSize: 14,
-        color: 'white',
+        color: "white",
         paddingBottom: 8,
         paddingTop: 8,
-        flex: 0.7,
+        flex:0.7
     },
 
     actions: {
@@ -170,20 +183,20 @@ const styles = StyleSheet.create({
         height: 40,
         paddingTop: 8,
         marginTop: 10,
-        gap: 16,
+        gap: 16
     },
 
     button: {
         flex: 1,
         height: 40,
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
         borderWidth: 2,
         borderRadius: 100,
     },
 
     buttonFirst: {
-        backgroundColor: '#1D192B',
+        backgroundColor: "#1D192B",
         borderColor: '#E8DEF8',
     },
 
@@ -202,18 +215,18 @@ const styles = StyleSheet.create({
         width: 12,
         height: 12,
         marginRight: 5,
-        marginTop: 3,
+        marginTop: 3
     },
 
     buttonContent: {
-        justifyContent: 'center',
-        flexDirection: 'row',
+        justifyContent: "center",
+        flexDirection: "row",
     },
 
     rowContainer: {
-        flexDirection: 'row',
-        gap: 14,
-    },
+        flexDirection: "row",
+        gap: 14
+    }
 });
 
 export default DropDown;
