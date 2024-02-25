3d798d98418e7c7af1ae04fb7cae87a90ebca98e
IraHrechyn <142835111+IraHrechyn@users.noreply.github.com>
2024-02-25 18:37:05 +0200

    Update Schedule.jsx

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -1,43 +1,56 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StatusBar, StyleSheet, ScrollView, Text, RefreshControl } from 'react-native';
 import Dropdown from './Dropdown.jsx';
-import DateBlock from './DateBlock.jsx';
-import { useState, useEffect } from 'react';
-import { getGroupSchedule } from '../../services/schedule-api.service.js';
-import { AvailableUni } from '../../shared/enums/available-uni.enum.js';
-import CreateDefaultModel from '../../services/create-default-model.js';
+import DateBlock from "./DateBlock";
+import CreateDefaultModel from "../../services/create-default-model";
+import { GetSchedule } from "../../services/local-storage.service";
+
 
 export default function Schedule() {
-    const [currentDate, setCurrentDate] = useState(new Date());
-    const [lastUpdateDate, setLastUpdateDate] = useState('');
-    const [lastUpdateTime, setLastUpdateTime] = useState('');
-    const [schedule, setSchedule] = useState([]);
-    const [refreshing, setRefreshing] = useState(false);
+    const [ currentDate, setCurrentDate ] = useState(new Date());
+    const [ lastUpdateDate, setLastUpdateDate ] = useState('');
+    const [ lastUpdateTime, setLastUpdateTime ] = useState('');
+    const [ schedule, setSchedule ] = useState([]);
+    const [ refreshing, setRefreshing ] = useState(false);
 
-    const { setFacultyId, setYear, setGroupId, data } = CreateDefaultModel();
+    const {
+        setFacultyId,
+        setYear,
+        setGroupId,
+        data
+    } = CreateDefaultModel();
 
     useEffect(() => {
         setFacultyId(1002);
         setYear('2023-2024-2');
         setGroupId(8567);
         fetchSchedule().then();
-    }, [currentDate]);
+    }, [ currentDate ]);
 
     const fetchSchedule = async () => {
         try {
             const formattedDate = currentDate.toISOString().split('T')[0];
             const paddedGroupId = String(data.groupId).padStart(6, '0');
-            const scheduleData = await getGroupSchedule(AvailableUni.PNU, paddedGroupId, data.year, data.facultyId);
+            // await SetSchedule(
+            //     AvailableUni.PNU,
+            //     paddedGroupId,
+            //     data.year,
+            //     data.facultyId,
+            // );
 
-            const filteredSchedule = scheduleData.filter((lesson) => lesson.d === formattedDate);
+            const schedule = await GetSchedule(paddedGroupId);
+
+
+            const filteredSchedule = schedule.filter(lesson => lesson.d === formattedDate);
             setSchedule(filteredSchedule);
+            
+            const dateDay = schedule[0].lastUpdate.dateDay;
+            const dateTime = schedule[0].lastUpdate.dateTime;
 
-            const updateDateTime = new Date();
-            const updateDate = updateDateTime.toLocaleDateString('uk-UA');
-            const updateTime = updateDateTime.toLocaleTimeString('uk-UA');
+            setLastUpdateDate(dateDay);
+            setLastUpdateTime(dateTime);
 
-            setLastUpdateDate(updateDate);
-            setLastUpdateTime(updateTime);
+
         } catch (error) {
             console.error('Error fetching schedule:', error);
         }
@@ -63,12 +76,20 @@ export default function Schedule() {
 
     return (
         <View style={styles.container}>
-            <DateBlock date={currentDate} onBackward={handleBackward} onForward={handleForward} />
-            <ScrollView contentContainerStyle={{ flexGrow: 1 }} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+            <ScrollView
+                contentContainerStyle={{ flexGrow: 1 }}
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
+            >
+                <DateBlock date={currentDate} onBackward={handleBackward} onForward={handleForward}/>
                 <View style={{ ...styles.container, marginTop: 60 }}>
-                    {schedule.map((lesson, index) => (
-                        <Dropdown key={index} lesson={lesson} />
-                    ))}
+                    <StatusBar style="auto"/>
+                    {schedule.length === 0 ? (
+                        <Text style={styles.noClassesText}>Сьогодні немає пар:)</Text>
+                    ) : (
+                        schedule.map((lesson) => (
+                            <Dropdown key={lesson.id} lesson={lesson}/>
+                        ))
+                    )}
                 </View>
             </ScrollView>
             <View>
@@ -79,7 +100,6 @@ export default function Schedule() {
         </View>
     );
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -93,4 +113,10 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         alignItems: 'flex-end',
     },
+    noClassesText:{
+        fontSize: 16,
+        margin: 10,
+        textAlign: 'center',
+        color: "white",
+    }
 });
