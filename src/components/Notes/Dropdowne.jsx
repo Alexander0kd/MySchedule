7eae32189ae12d4b97f2182a2ebb0523b5d52333
Kostia Semkiv <semksvk@gmail.com>
2024-02-29 13:19:34 +0200

    feat: added dropdown more menu

--- src/components/Notes/Dropdowne.jsx
+++ src/components/Notes/Dropdowne.jsx
@@ -5,6 +5,9 @@ import Arrow from '../../../assets/arrow_drop_down.png';
 import Bell from '../../../assets/bell.png';
 import More from '../../../assets/more_vert.png';
 import Plus from '../../../assets/plus-icon.png';
+import Edit from '../../../assets/edit.png';
+import Trash from '../../../assets/trashcan.png';
+
 import { ScrollView } from 'react-native-gesture-handler';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
@@ -15,6 +18,12 @@ const Dropdown = ({ note }) => {
     const toggleDropdown = () => {
         setDropdownVisible(!isDropdownVisible);
         setArrowRotation(arrowRotation === 0 ? 180 : 0);
+    };
+
+    const [isMenuVisible, setMenuVisible] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuVisible(!isMenuVisible);
     };
     const [notes, setNotes] = useState([]);
     useEffect(() => {
@@ -41,10 +50,22 @@ const Dropdown = ({ note }) => {
                             <View key={singleNote.id} style={styles.notesCont}>
                                 <View style={styles.dataSettingContainer}>
                                     <Text style={styles.dataText}>{singleNote.data}</Text>
-                                    <TouchableOpacity style={styles.touchableCont}>
+                                    <TouchableOpacity style={styles.touchableCont} onPress={toggleMenu}>
                                         <Image source={More} style={styles.iconMore} />
                                     </TouchableOpacity>
                                 </View>
+                                {isMenuVisible && (
+                                    <View style={styles.menuContainer}>
+                                        <TouchableOpacity style={styles.menuButton} onPress={() => console.log('Option 1')}>
+                                            <Image source={Edit} style={styles.iconMoreMenu} />
+                                            <Text style={styles.menuButtonText}>Редагувати</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity style={styles.menuButton} onPress={() => console.log('Option 2')}>
+                                            <Image  source={Trash} style={[styles.iconMoreMenu,{tintColor: '#DC362E'}]} />
+                                            <Text style={[styles.menuButtonText,{color: '#DC362E'}]}>Видалити</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                )}
                                 <ScrollView style={{ maxHeight: 220 }}>
                                     <Text style={styles.infoText}>{singleNote.text}</Text>
                                 </ScrollView>
@@ -86,6 +107,9 @@ const styles = StyleSheet.create({
         fontSize: 14,
         paddingBottom: 4,
         flex: 1,
+    },
+    redBut: {
+        backgroundColor: '#DC362E',
     },
     notesCont: {
         width: '100%',
@@ -177,7 +201,7 @@ const styles = StyleSheet.create({
     },
 
     buttonSecond: {
-        backgroundColor: '#6750A4',
+        backgroundColor: '#6750a4',
         borderWidth: 1,
         borderColor: '#6750A4',
     },
@@ -197,6 +221,13 @@ const styles = StyleSheet.create({
         width: 4,
         height: 16,
         marginRight: 5,
+        resizeMode: 'contain',
+    },
+    iconMoreMenu: {
+        position: 'absolute',
+        left: 12,
+        width: 24,
+        height: 24,
         resizeMode: 'contain',
     },
     touchableCont: {
@@ -220,6 +251,32 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         gap: 14,
+    },
+    menuContainer: {
+        position: 'absolute',
+        top: 0,
+        right: 30,
+        width: 160,
+        height: 128,
+        backgroundColor: '#211F26',
+        borderRadius: 10,
+        zIndex: 1, // Ensure the menu appears above other elements
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    menuButton: {
+        flex: 1,
+        flexDirection: 'row',
+        width: '100%',
+        height: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'left',
+    },
+    menuButtonText: {
+        color: 'white',
+        fontSize: 14,
+        left: 12,
     },
 });
 
