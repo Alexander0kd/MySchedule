be25d274bad2d09fbad83b98fae071774f6344e1
Kademsky Alexander <79721736+Buzz12345@users.noreply.github.com>
2024-03-02 17:42:09 +0200

    fix: delete unnecessary return type

--- src/shared/components/ProfileForm.tsx
+++ src/shared/components/ProfileForm.tsx
@@ -124,7 +124,7 @@ export const ProfileForm: FunctionComponent<{
 
         // Update Group List
         if (university && faculty && year) {
-            const updateGroupList = async (): Promise<void> => {
+            const updateGroupList = async () => {
                 const displayedGroups = [];
 
                 await getGroupList(UniEndpoints[university], Number(faculty), Number(year)).then((group: IGroup[]) => {
