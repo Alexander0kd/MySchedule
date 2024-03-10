616aceee005abc3f478ee3ac7395fa7aa28fe50a
Kademsky Alexander <79721736+Buzz12345@users.noreply.github.com>
2024-03-10 23:03:48 +0200

    feat: added profile editing

--- src/components/Settings/Profile/ProfileEdit/ProfileEdit.tsx
+++ src/components/Settings/Profile/ProfileEdit/ProfileEdit.tsx
@@ -5,72 +5,92 @@ import { AvailableRoutes } from '../../../../shared/env/available-routes';
 import { LoadingScreen } from '../../../../shared/components/LoadingScreen';
 import {StackNavigationProp} from "@react-navigation/stack";
 import {IProfile} from "../../../../shared/interfaces/profile.interface";
-import {addProfile, setActiveProfile} from "../../../../services/local-storage.service";
+import {getProfileById, setActiveProfile, updateProfileById} from "../../../../services/local-storage.service";
 import {handleError, openModal} from "../../../../services/utility.service";
 import {ProfileForm} from "../../../../shared/components/ProfileForm";
 import {ThinButton} from "../../../../shared/components/ThinButton";
 import {RoundButton} from "../../../../shared/components/RoundButton";
 
-
 export const ProfileEdit: FunctionComponent<{
-    route: RouteProp<AvailableRoutes>;
-
+    route: RouteProp<AvailableRoutes>
 }> = (props) => {
     const navigation: StackNavigationProp<AvailableRoutes> = useNavigation();
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [filledProfile, setFilledProfile] = useState<IProfile>(null);
 
     const [isFormFilled, setIsFormFilled] = useState<boolean>(false);
     const [profileData, setProfileData] = useState<IProfile>(null);
 
     const handleSave = async (profileData: IProfile) => {
         try {
-            const profileAdded = await addProfile(profileData);
-            if (profileAdded) {
+            const profileUpdated = await updateProfileById(props.route.params.profileId, profileData, false);
+            if (profileUpdated) {
                 await setActiveProfile(profileData.id);
-                navigation.goBack();
+                navigation.push('AppNavbar');
             }
         } catch (error) {
             handleError(error);
         }
     };
+
     useEffect(() => {
-        navigation.addListener('beforeRemove', (e) => {
+        const loadData = async () => {
+            setIsLoading(true);
+            const profile = await getProfileById(props.route.params.profileId);
+            if (profile) {
+                setFilledProfile(profile);
+            } else {
+                navigation.push('ProfileAdd');
+            }
+            setIsLoading(false);
+        }
+
+        loadData();
+
+        navigation.addListener('beforeRemove', async (e) => {
             e.preventDefault();
 
-            const modal = openModal('Бажаєте скасувати редагування?', 'Цю дію неможливо відмінити', 'Ні, залишитись', 'Так, скасувати');
+            const modal = await openModal(
+                'Бажаєте скасувати редагування?',
+                'Цю дію неможливо відмінити',
+                'Ні, залишитись',
+                'Так, скасувати'
+            );
 
             if (modal) {
                 navigation.dispatch(e.data.action);
             }
         });
     }, []);
-    if (props.route.params && props.route.params.profileId) {
 
-        return (
-            <View style={styles.section}>
-                <View style={styles.container}>
-                    <Text style={styles.subTitle}>Змініть необхідну інформацію</Text>
-                    <ProfileForm setIsFormFilled={setIsFormFilled} setProfileData={setProfileData} />
-                </View>
-                <View style={styles.wrapper}>
-                    <ThinButton
-                        label="Скасувати"
-                        onPressFunc={() => {
-                            navigation.goBack();
-                        }}
-                    />
-                    <RoundButton
-                        label="Зберегти"
-                        onPressFunc={() => {
-                            handleSave(profileData);
-                        }}
-                        disabled={!isFormFilled}
-                    />
-                </View>
-            </View>
-        );
+    if (isLoading || !props.route.params || !props.route.params.profileId) {
+        return <LoadingScreen></LoadingScreen>;
     }
 
-    return <LoadingScreen></LoadingScreen>;
+    return (
+        <View style={styles.section}>
+            <View style={styles.container}>
+                <Text style={styles.subTitle}>Змініть необхідну інформацію</Text>
+                <ProfileForm setIsFormFilled={setIsFormFilled} setProfileData={setProfileData} filledProfile={filledProfile} />
+            </View>
+            <View style={styles.wrapper}>
+                <ThinButton
+                    label="Скасувати"
+                    onPressFunc={() => {
+                        navigation.replace('ProfileList');
+                    }}
+                />
+                <RoundButton
+                    label="Зберегти"
+                    onPressFunc={() => {
+                        handleSave(profileData);
+                    }}
+                    disabled={!isFormFilled}
+                />
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
