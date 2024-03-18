9647df2dec5bb97c75c3f58cd06b48d740478c77
IraHrechyn <142835111+IraHrechyn@users.noreply.github.com>
2024-03-18 09:18:01 +0200

    Update subject.interface.ts

--- src/shared/interfaces/subject.interface.ts
+++ src/shared/interfaces/subject.interface.ts
@@ -1,1 +1,9 @@
-
+/**
+ * @interface ISchedule Представляє предмет.
+ * @param {string} l - Назва предмету.
+ * @param {boolean} v - Видимість предмету.
+ */
+export interface ISubject {
+    l: string;
+    v: boolean;
+}
