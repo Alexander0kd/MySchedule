b8a06b1355ce093d51c6a4b1472e8f5f331189ff
IraHrechyn <142835111+IraHrechyn@users.noreply.github.com>
2024-03-20 01:23:33 +0200

    Update Notification.tsx

--- src/components/Settings/Notification/Notification.tsx
+++ src/components/Settings/Notification/Notification.tsx
@@ -1,10 +1,22 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import {ScheduleChanges} from "./schedule-changes";
+import {DropDown} from "./dropdown-pairing-notifications";
+import RemindersList from "./reminders-list";
+
+
+const testData = [
+    { date: '13 грудня', time: '9:00', subject: 'Математика' },
+    { date: '14 грудня', time: '10:30', subject: 'Фізика' },
+    { date: '15 грудня', time: '12:00', subject: 'Історіяzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz' },
+];
 
 export const Notification = () => {
     return (
         <View>
-            <Text>Налаштування Сповіщень</Text>
+            <ScheduleChanges/>
+            <DropDown />
+            <RemindersList lessons={testData}/>
         </View>
     );
 };
