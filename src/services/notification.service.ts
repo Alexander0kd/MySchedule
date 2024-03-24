59f67a748012414ba025898d300d15430f17c6de
Kademsky Alexander <79721736+Buzz12345@users.noreply.github.com>
2024-03-24 17:48:53 +0200

    feat: added notify by function

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,13 +1,17 @@
 import { IProfile } from "../shared/interfaces/profile.interface";
 import { NotificationItem } from "../shared/interfaces/settings.interface";
 
-export function handleNotificationUpdate(profile: IProfile) {
-    handleNotifyChanges(profile.settings.notification.notifyChanges);
-    handleNotifyBy(profile.settings.notification.notifyBy);
-    handleNotifyList(profile.settings.notification.notificationList);
+import * as Notifications from 'expo-notifications';
+import { ISchedule } from "../shared/interfaces/schedule.interface";
+import { handleError } from "./utility.service";
+
+export async function handleNotificationUpdate(profile: IProfile) {
+    await handleNotifyChanges(profile.settings.notification.notifyChanges);
+    await handleNotifyBy(profile.schedule, profile.settings.notification.notifyBy);
+    await handleNotifyList(profile.settings.notification.notificationList);
 }
 
-function handleNotifyChanges(notifyChanges: boolean) {
+async function handleNotifyChanges(notifyChanges: boolean) {
     //! Step 1: Очистити всі BackgroundFetch
     //! Step 2: Якщо notifyChanges == true, тоді добавити оновлення в фоні щодня у рандомному проміжку між 20:00 і 21:00 із таким алгоритмом:
     //! 1. const profile await getActiveProfile();
@@ -15,16 +19,43 @@ function handleNotifyChanges(notifyChanges: boolean) {
     //! 3. if (data && data.length > 0 && profile.schedule !== data) { * сповістити користувача про оновлення у розкладі* }
 }
 
-function handleNotifyBy(notifyBy: number) {
-    //! Step 1: Очистити всі сповіщення в задньому фоні
-    //! Step 2: Якщо notifyBy != 0, тоді добавити оновлення в фоні до кожного заняття за notifyBy хвилин. Спосіб отримання списку занять:
-    //! 1. const profile await getActiveProfile();
-    //! 2. const data = profile.schedule;
-    //! Годину заняття можна знайти як profile.schedule[i].ls
-    //! Дату заняття можна знайти як profile.schedule[i].d - Дата заняття у форматі РРРР-ММ-ДД.
+async function handleNotifyBy(schedule: ISchedule[], notifyBy: number) {
+    await Notifications.cancelAllScheduledNotificationsAsync();
+
+    if (notifyBy !== 0) {
+        const currentDate = new Date();
+
+        Notifications.setNotificationHandler({
+            handleNotification: async () => ({
+                shouldShowAlert: true,
+                shouldPlaySound: true,
+                shouldSetBadge: true,
+            }),
+        });
+
+        schedule.forEach(async (item) => {
+            const itemDate = new Date(item.d + 'T' + item.ls);
+
+            if (itemDate >= currentDate) {
+                try {                    
+                    await Notifications.scheduleNotificationAsync({
+                        content: {
+                          title: 'Скоро ропочнеться заняття!',
+                          body: `"${item.l}" розпочнеться о ${item.ls}.\n Не пропусти!`,
+                        },
+                        trigger: {
+                            date: itemDate.getTime() - (notifyBy * 60 * 1000),
+                        },
+                    });
+                } catch (error) {
+                    handleError(error);
+                }
+            }
+        });
+    }
 }
 
-function handleNotifyList(notificationList: NotificationItem[]) {
+async function handleNotifyList(notificationList: NotificationItem[]) {
     //! Step 1: Очистити всі сповіщення в задньому фоні
     //! Step 2: Якщо notificationList.length > 0, тоді добавити будильник в телефоні на годину, котра вказана у notificationList[i].date
 }
