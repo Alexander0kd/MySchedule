7e06af953fac279eed140c37071a6983cd6ca543
Kademsky Alexander <79721736+Buzz12345@users.noreply.github.com>
2024-03-24 19:17:06 +0200

    feat: added handleNotifyChanges

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,9 +1,55 @@
+import * as Notifications from 'expo-notifications';
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager'
+
 import { IProfile } from "../shared/interfaces/profile.interface";
 import { NotificationItem } from "../shared/interfaces/settings.interface";
 
-import * as Notifications from 'expo-notifications';
 import { ISchedule } from "../shared/interfaces/schedule.interface";
 import { handleError } from "./utility.service";
+
+import { getGroupSchedule } from "./schedule-api.service";
+import { UniEndpoints } from "../shared/universities/uni-endpoints.enum";
+import { getActiveProfile } from './local-storage.service';
+
+const BACKGROUND_TASK_NAME = 'scheduleUpdateTask';
+
+TaskManager.defineTask(BACKGROUND_TASK_NAME, async () => {
+    const now = new Date();
+    if (now.getHours() < 12) {
+        return BackgroundFetch.BackgroundFetchResult.NoData;
+    }
+
+    try {
+        const profile = await getActiveProfile();
+        const data = await getGroupSchedule(UniEndpoints[profile.university], profile.faculty, profile.group);
+        
+        if (data && data.length > 0 && profile.schedule !== data) {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true,
+                }),
+            });
+
+            await Notifications.scheduleNotificationAsync({
+                content: {
+                  title: 'Розклад оновився!',
+                  body: 'Перевірьте ваш оновлений розклад!',
+                },
+                trigger: null
+            });
+
+            return BackgroundFetch.BackgroundFetchResult.NewData;
+        }
+        
+        return BackgroundFetch.BackgroundFetchResult.NoData;
+    } catch (error) {
+        return BackgroundFetch.BackgroundFetchResult.Failed;
+    }
+});
+
 
 export async function handleNotificationUpdate(profile: IProfile) {
     await handleNotifyChanges(profile.settings.notification.notifyChanges);
@@ -12,50 +58,59 @@ export async function handleNotificationUpdate(profile: IProfile) {
 }
 
 async function handleNotifyChanges(notifyChanges: boolean) {
-    //! Step 1: Очистити всі BackgroundFetch
-    //! Step 2: Якщо notifyChanges == true, тоді добавити оновлення в фоні щодня у рандомному проміжку між 20:00 і 21:00 із таким алгоритмом:
-    //! 1. const profile await getActiveProfile();
-    //! 2. const data = await getGroupSchedule(UniEndpoints[profile.university], profile.faculty, profile.group);
-    //! 3. if (data && data.length > 0 && profile.schedule !== data) { * сповістити користувача про оновлення у розкладі* }
+    if (!notifyChanges) {
+        const isRegistered = await TaskManager.isTaskRegisteredAsync(BACKGROUND_TASK_NAME);
+        if (isRegistered) {
+            await BackgroundFetch.unregisterTaskAsync(BACKGROUND_TASK_NAME);
+        }
+        return;
+    }
+
+    await BackgroundFetch.registerTaskAsync(BACKGROUND_TASK_NAME, {
+        minimumInterval: 12 * 60 * 60,
+        stopOnTerminate: false,
+        startOnBoot: true,
+    });
 }
 
 async function handleNotifyBy(schedule: ISchedule[], notifyBy: number) {
     await Notifications.cancelAllScheduledNotificationsAsync();
 
-    if (notifyBy !== 0) {
-        const currentDate = new Date();
+    if (notifyBy === 0) return;
 
-        Notifications.setNotificationHandler({
-            handleNotification: async () => ({
-                shouldShowAlert: true,
-                shouldPlaySound: true,
-                shouldSetBadge: true,
-            }),
-        });
+    Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+            shouldShowAlert: true,
+            shouldPlaySound: true,
+            shouldSetBadge: true,
+        }),
+    });
 
-        schedule.forEach(async (item) => {
-            const itemDate = new Date(item.d + 'T' + item.ls);
+    const currentDate = new Date();
+    schedule.forEach(async (item) => {
+        const itemDate = new Date(item.d + 'T' + item.ls);
 
-            if (itemDate >= currentDate) {
-                try {                    
-                    await Notifications.scheduleNotificationAsync({
-                        content: {
-                          title: 'Скоро ропочнеться заняття!',
-                          body: `"${item.l}" розпочнеться о ${item.ls}.\n Не пропусти!`,
-                        },
-                        trigger: {
-                            date: itemDate.getTime() - (notifyBy * 60 * 1000),
-                        },
-                    });
-                } catch (error) {
-                    handleError(error);
-                }
+        if (itemDate >= currentDate) {
+            try {                    
+                await Notifications.scheduleNotificationAsync({
+                    content: {
+                        title: 'Скоро ропочнеться заняття!',
+                        body: `"${item.l}" розпочнеться о ${item.ls}.\n Не пропусти!`,
+                    },
+                    trigger: {
+                        date: itemDate.getTime() - (notifyBy * 60 * 1000),
+                    },
+                });
+            } catch (error) {
+                handleError(error);
             }
-        });
-    }
+        }
+    });
 }
 
 async function handleNotifyList(notificationList: NotificationItem[]) {
+    notificationList = [];
+    console.log(notificationList);
     //! Step 1: Очистити всі сповіщення в задньому фоні
     //! Step 2: Якщо notificationList.length > 0, тоді добавити будильник в телефоні на годину, котра вказана у notificationList[i].date
 }
